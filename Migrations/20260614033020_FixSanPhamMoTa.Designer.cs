472d82899e979cf876fc2baeec2bd385a10fa7b9
Bommner69 <vinhprop2004@gmail.com>
2026-06-14 11:53:42 +0700

    fix: add missing migrations for DaXacThuc, DiaChi, and fix SanPham.MoTa type
    - Apply AddXacThucEmailAndDaXacThuc migration (adds DaXacThuc column, XacThucEmail table)
    - Apply AddDiaChiToNguoiDung migration (adds DiaChi column)
    - Apply FixSanPhamMoTa migration (nvarchar(2000) -> nvarchar(max))
    - AvatarUrl column already existed in DB, excluded from migration

--- Migrations/20260614033020_FixSanPhamMoTa.Designer.cs
+++ Migrations/20260614033020_FixSanPhamMoTa.Designer.cs
@@ -4,6 +4,7 @@ using ECommerceFinalProject.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECommerceFinalProject.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260614033020_FixSanPhamMoTa")]
+    partial class FixSanPhamMoTa
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
